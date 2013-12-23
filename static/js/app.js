0decde9a1c5ee206ae11982fb5759f201b5c6ab5
Edd Turtle <edd@designedbyaturtle.co.uk>
2013-12-23 20:40:01 +0000

    Move JS into external File

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,28 @@
+var pastWeek = [];
+
 $(document).ready( function() {
-
-
-
-
 	var d = new Date().getTime();
 	$.ajax({
-		url: 'api/v1/0/' + d,
+		url: 'api/v1/0/0',
 		type: 'GET',
+		dataType: 'json',
+		contentType: 'application/json',
 		success: function(json) {
-			console.log(json)
+			var latest = json.instances[0];
+			$('.latest-data .temperature').html( (latest.temp_instance/1000).toFixed(1) + '&deg;c' );
+			$('.latest-data .date').html( latest.temp_date );
+
+			fillTable(json.instances, $('.past-week'));
 		}
-	})
-
-
-
-
-
+	});
 });
+
+function fillTable(instances, table) {
+	$.each(instances, function(i, val) {
+		var html = '<tr>';
+		html += '<td>' + val.temp_date + '</td>';
+		html += '<td>' + (val.temp_instance/1000).toFixed(1) + '&deg;c</td>';
+		html += '</tr>';
+		table.append(html);
+	});
+}
